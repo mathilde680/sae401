665d29c5e3079df104e82f5677e49b3647c2a4a3
Github MMI3 <github@mmi-troyes.fr>
2025-03-14 14:08:16 +0000

    Ajout grille Controller + SCSS

--- src/Form/AjoutEvaluationType.php
+++ src/Form/AjoutEvaluationType.php
@@ -7,6 +7,7 @@ use App\Entity\Matiere;
 use App\Entity\Professeur;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -21,9 +22,24 @@ class AjoutEvaluationType extends AbstractType
             ])
             ->add('coef')
 
-            ->add('statut')
+            ->add('statut', ChoiceType::class, [
+                'choices'  => [
+                    'Publiée' => 'Publiée',
+                    'En cours' => 'En_cours',
+                    'Enregistrée' => 'Enregistrée',
+                ],
+                'expanded' => false, // false = liste déroulante, true = boutons radio
+                'multiple' => false, // false = choix unique, true = sélection multiple
+            ])
 
-            ->add('statut_groupe')
+            ->add('statut_groupe', ChoiceType::class, [
+                'choices'  => [
+                    'Groupe' => 'groupe',
+                    'Individuel' => 'individuel',
+                ],
+                'expanded' => false, // false = liste déroulante, true = boutons radio
+                'multiple' => false, // false = choix unique, true = sélection multiple
+            ])
 
             ->add('taille_max_groupe')
 
